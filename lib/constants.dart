59907329282bb3e094e6c1104360512d100b5c65
Andrei Bobkov <slider7259@gmail.com>
2025-02-13 11:36:26 +0100

    feat(constants): auth token

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,8 @@
 class Constants {
-  static const String medusaApiUrl = 'https://srh-vending-locker-medusa.ctoofeverything.dev';
-  static const String medusaApiKey = 'pk_ba676698cd095c8532b472e970cee4700a1e7c1d7bbbc4105cb0329a5825f3fb';
+  static const String medusaApiUrl =
+      'https://srh-vending-locker-medusa.ctoofeverything.dev';
+  static const String medusaApiKey =
+      'pk_ba676698cd095c8532b472e970cee4700a1e7c1d7bbbc4105cb0329a5825f3fb';
   static const String berlinCampusRegionId = 'reg_01JEPF0D5S5A5W36RMC70XHM2Y';
   static const String salesChannelId = 'sc_01JEP8K8J2EKZKS44HEM1KKMNR';
   static const Map<String, String> locationIds = {
@@ -9,4 +11,7 @@ class Constants {
     "sloc_01JJKXD885SGMRBXK2QZKMH0KN": "SHED A2",
     "sloc_01JJKXE2T8B1PWHRQF23ED7W7J": "HALL A1"
   };
+  // POST /auth/customer/emailpass email=slider7259@gmail.com password=123123123
+  static const String customerAuthToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY3Rvcl9pZCI6IiIsImFjdG9yX3R5cGUiOiJjdXN0b21lciIsImF1dGhfaWRlbnRpdHlfaWQiOiJhdXRoaWRfMDFKRVA5OVNaV0VaRzhLUFBXQ1E4V05CV1MiLCJhcHBfbWV0YWRhdGEiOnt9LCJpYXQiOjE3Mzk0NDI5MTAsImV4cCI6MTczOTUyOTMxMH0.pN0sIja_zvtvqMKpNQWa5TDSSCnSmlRsqoNUh6p1y6E';
 }
